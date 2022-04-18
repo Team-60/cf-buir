d6e931f366c654c3934c1574ad763cb07af359ae
Rupanshu Yadav <58172798+zyrch@users.noreply.github.com>
2022-04-18 12:42:15 +0530

    change model type with args

--- buir/options.py
+++ buir/options.py
@@ -6,6 +6,7 @@ def args_parser():
     parser.add_argument("--exp_name", type=str, default="default", help="experiment name")
     parser.add_argument("--exp_disc", type=str, default="default", help="description")
 
+    parser.add_argument("--model", type=str, default='buir-id', help="type of model used")
     parser.add_argument("--latent_size", type=int, default=250, help="latent embeddings size")
     parser.add_argument("--epochs", type=int, default=50, help="rounds of training")
     parser.add_argument("--lr", type=float, default=0.001, help="learning rate")
@@ -14,6 +15,6 @@ def args_parser():
     parser.add_argument("--momentum", type=float, default=0.995, help="target encoder momentum")
     parser.add_argument("--train_ratio", type=float, default=0.8, help="train split ratio")
 
-    parser.add_argument("--num_workers", type=int, default=2, help="num workers dataloader")
+    parser.add_argument("--num_workers", type=int, default=0, help="num workers dataloader")
     args = parser.parse_args()
     return args
